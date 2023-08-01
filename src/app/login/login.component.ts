10e634c6795cc43a1b5bc2048ecba05ff368c5d0
jamiromic <jamiromic@gmail.com>
2023-08-01 16:28:34 +0200

    init

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  userId: string = "";
+  password: string = "";
+
+  autenticato: boolean = true;
+  consentito: boolean = false;
+  errorMsg: string = "L'userId e/o la password sono errati"
+  
+  constructor(private route: Router) {
+
+  }
+
+  gestAuth = (): void => {
+    console.log(this.userId,this.password);
+
+    if(this.userId === "jamiromic" && this.password === "jimenez0486") {
+      this.autenticato = true;
+      this.route.navigate(['welcome', this.userId])
+    } else {
+      this.autenticato = false;
+    }
+  }
+
+}
